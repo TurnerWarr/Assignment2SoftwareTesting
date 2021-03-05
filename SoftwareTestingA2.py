aea0906bbd33a942fab3c7a571c1a9d5a1d57cd4
TurnerWarr <79030326+TurnerWarr@users.noreply.github.com>
2021-03-04 21:00:23 -0600

    Add files via upload

--- SoftwareTestingA2.py
+++ SoftwareTestingA2.py
@@ -1,44 +1,6 @@
-
+from Retire import *
+from BMI import *
 import string;
-#TODO
-def BMI(height_ft, height_in, weight_lbs):
-    height_ft = height_ft * 12;
-    height_in += height_ft;
-    weight_metric = weight_lbs * 0.45;
-    height_metric_in = height_in * 0.025;
-    height_metric_in = height_metric_in ** 2;
-    stepfour = weight_metric / height_metric_in
-    stepfour = round(stepfour, 1);
-    if(stepfour < 18.5):
-        index = "Underweight: ";
-        index += str(stepfour);
-    elif(stepfour >= 30):
-        index = "Obese: ";
-        index += str(stepfour);
-    elif(stepfour >= 18.5 and stepfour <= 24.9):
-        index = "Normal Weight: ";
-        index += str(stepfour);
-    elif(stepfour >= 25 and stepfour <= 29.9):
-        index = "Overweight: ";
-        index += str(stepfour);
-    return index;
-
-#TODO
-def Retire(current_age, annual_salary, perc_save, desired_goal):
-    saving = annual_salary * perc_save;
-    employ = saving * 0.35;
-    saving += employ;
-    goal = desired_goal / saving;
-    goal = round(goal, 0);
-    if(goal + current_age >= 100):
-        out = "Goal cannot be met with current parameters ";
-        out += str(goal);
-    elif(goal + current_age < 100):
-        out = "Goal can be met in ";
-        out += str(goal);
-        out += " years";
-    return out;
-
 
 def main():
     #initialize container
